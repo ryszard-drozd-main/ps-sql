31741f392de80cc87ada04f99465cf99a2a5987b
Ryszard Drozd <csharp@poczta.onet.pl>
2020-11-20 14:26:26 +0100

    Formatting

--- WithOutParameters.sql
+++ WithOutParameters.sql
@@ -1,17 +1,17 @@
 create or alter procedure WithOutParameters
-@RandResult int output
-,@RandResult2 int output
-,@TextResult varchar(200) output
+    @RandResult int output
+    ,@RandResult2 int output
+    ,@TextResult varchar(200) output
 as
 begin
-declare @t1 table (id int, fname varchar(100))
-declare @t2 table (id int, fname varchar(100))
-insert into @t1 (id,fname) values (1,'Ala'),(2,'Beata')
-insert into @t2 (id,fname) values (1,'Roman'),(2,'Kajetan')
-select * from @t1;
-select * from @t2;
-select @RandResult = 10 * rand();
-select @RandResult2 = 10 * rand();
-select @TextResult='Jadą dzieci jadą koło mego sadu';
-return 10 * rand();
+    declare @t1 table (id int, fname varchar(100))
+    declare @t2 table (id int, fname varchar(100),lname varchar(100))
+    insert into @t1 (id,fname) values (1,'Ala'),(2,'Beata')
+    insert into @t2 (id,fname,lname) values (1,'Roman','Git'),(2,'Kajetan','Tfs')
+    select * from @t1;
+    select * from @t2;
+    select @RandResult = 10 * rand();
+    select @RandResult2 = 10 * rand();
+    select @TextResult='Jadą dzieci jadą koło mego sadu';
+    return 10 * rand();
 end;
